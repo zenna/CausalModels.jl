099e1120690a323e956ebc66cd21b53333ceb214
Allen Yang <allenyang@MacBook-Pro-6.local>
2020-03-21 14:11:03 -0700

    fixed comments and updated grammar on language task

--- src/AllensCausalModel/languagetask/semlang.jl
+++ src/AllensCausalModel/languagetask/semlang.jl
@@ -15,34 +15,22 @@ SEMSyntaxError() = SEMSyntaxError("")
 parse exogenous variable
 """
 function parseexo(line)
-  :($(line.args[2])=ExogenousVariable($(Meta.quot(line.args[2])),$(line.args[3])))
+  :($(line.args[2]) = ExogenousVariable($(Meta.quot(line.args[2])),$(line.args[3])))
 end
 """
 parse endogenous variable
 """
 function parseendo(line)
-  subexpr=line.args[2]
-  if typeof(subexpr)==Symbol
-    return :($(line.args[1])=EndogenousVariable(identity,($(subexpr),)))
-  elseif length(subexpr.args)==2
-    return :($(line.args[1])=EndogenousVariable($(subexpr.args[1]),($(subexpr.args[2]),)))
+  subexpr = line.args[2]
+  if typeof(subexpr) == Symbol
+    return :($(line.args[1]) = EndogenousVariable(identity,($(subexpr),)))
+  elseif length(subexpr.args) == 2
+    return :($(line.args[1]) = EndogenousVariable($(subexpr.args[1]),($(subexpr.args[2]),)))
   else
-    return :($(line.args[1])=EndogenousVariable($(subexpr.args[1]),($(subexpr.args[2]),$(subexpr.args[3]))))
+    return :($(line.args[1]) = EndogenousVariable($(subexpr.args[1]),($(subexpr.args[2]),$(subexpr.args[3]))))
   end
   #:($(line.args[1])=ExogenousVariable())
   line.args[2]
-end
-macro SEM(sem)
-  for i in 1:length(sem.args)
-    if i%2==0
-      line=sem.args[i]
-      if line.args[1]==:~
-        println(parseexo(line))
-      else
-        println(parseendo(line))
-      end
-    end
-  end
 end
 """
 Structural Equation Model
@@ -52,27 +40,29 @@ macro SEM(sem)
     throw(SEMSyntaxError("@SEM expects a block expression as input but was passed a "))
   end
   semlines = Expr[]
-  for i in 1:length(sem.args)
-    if i%2==0
-      line=sem.args[i]
-      if line.args[1]==:~
+  for line in sem.args
+    if !(line isa LineNumberNode)
+      if line.args[1] == :~
         push!(semlines,parseexo(line))
-      else
+      elseif line.head == :(=)
         push!(semlines,parseendo(line))
+      else
+        throw(SEMSyntaxError("invalid line in block"))
       end
     end
   end
   esc(Expr(:block, semlines...))
 end
 
-end
 # # Grammar  FINISHME
 # An SEM model is an `expr` defiend by the following grammar:
 
 """
 Prim        := Bernoulli | Uniform | Normal | ...
+Dist        := Prim(Number)
 Variable    := Symbol|Number
 unaryop     := !,-
 binaryop    := + | - | * | / | > | >= | <= | < | ...
-expr        := Symbol=unaryop Variable| Symbol=Variable binaryop Variable|Symbol~Prim
+line        := Symbol=unaryop Variable| Symbol=Variable binaryop Variable|Symbol~Dist
+expr        := line|line \n expr
 """
